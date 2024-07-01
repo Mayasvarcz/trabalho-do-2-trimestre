cf3a43b79f5a5da9cd246fc7dafa842144b51885
Mayasvarcz <130562862+Mayasvarcz@users.noreply.github.com>
2024-07-01 10:27:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+   ];`
+   let atual = 0;
+   let perguntaAtual;
+   function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+}
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta()
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+const alternativa
+(const alternativa of perguntaAtual.alternativas)
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+    }
+  }
+  function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+    }
+  }
+  function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativa.textContent = alternativa;
+    }
+  }
+  function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativa.textContent = alternativa;
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+        
